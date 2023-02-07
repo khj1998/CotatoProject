dde85c4aac52531c7764e021d6409cf4994d975f
kimhojin98@naver.com <kimhojin98@naver.com>
2023-02-07 16:57:24 +0900

    ENH : Front Merged

---
+++ src/main/java/cotato/CotatoApplication.java
@@ -0,0 +1,13 @@
+package cotato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotatoApplication.class, args);
+	}
+
+}
