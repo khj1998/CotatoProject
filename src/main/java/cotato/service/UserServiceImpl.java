cf9a4c1ae625c75d5621ff6f0f965cd8219f2174
kimhojin98@naver.com <kimhojin98@naver.com>
2023-02-09 00:25:39 +0900

    ENH: datasource설정, saveUser method추가

--- src/main/java/cotato/service/UserServiceImpl.java
+++ src/main/java/cotato/service/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package cotato.service;
 
 import cotato.dto.UserDto;
+import cotato.exception.UserAlreadyExistsException;
 import cotato.repository.UserRepository;
 import cotato.vo.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -16,8 +19,23 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public UserDto saveUser(UserDto userDto) {
+
+        boolean isExists = checkUserExists(userDto.getUserId());
+
+        if (isExists) {
+            throw new UserAlreadyExistsException(String.format("User %s already exists", userDto.getUserId()));
+        } else {
+            ModelMapper mapper = new ModelMapper();
+            mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+            User user = mapper.map(userDto,User.class);
+            userRepository.save(user);
+        }
+
         return userDto;
     }
 
-
+    private boolean checkUserExists(String userId) {
+        User user = userRepository.findByUserId(userId);
+        return user!=null;
+    }
 }
